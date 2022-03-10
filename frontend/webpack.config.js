7a8ef621cd93c776e44cdd143a411d1c3d3f3719
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-03-10 17:19:23 -0300

    05 | Create index.html and run the project

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,16 +2,16 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: './src/index/jsx',
+    entry: './src/index.jsx',
     output: {
         path: __dirname + '/public',
-        filename: './app.js'
+        filename: './app.js',
     },
     devServer: {
         port: 8080,
         contentBase: './public'
     },
-    resolver: {
+    resolve: {
         extensions: ['', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules'
