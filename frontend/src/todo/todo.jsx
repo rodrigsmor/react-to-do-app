9ad74beabf47e96d0289f435949666779fcdf95c
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-03-11 13:21:15 -0300

    07 | Create Page Header component

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import PageHeader from '../template/PageHeader';
 
 export default class Todo extends Component {
     render() {
         return (
             <div>
-                <h1>To-do</h1>
+                <PageHeader 
+                    name={'Tarefas'}
+                    small={'Cadastro'}
+                />
             </div>
         );
     }
